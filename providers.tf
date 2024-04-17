34bc0b1069f84a6199eeaadaef440276600107ca
adambaksa <151851619+adambaksa@users.noreply.github.com>
2024-04-17 14:07:34 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "bf9b83d8-67d1-4691-9cb8-3de759b0fa86"
-  client_id       = "383dcc0f-3f11-44c1-8b29-032f7f25ad7a"
-  client_secret   = "~ec8Q~Er1LCv_8nKZZDqmS-tFV~-bi.3x_r.rbq7"
-  tenant_id       = "e70d904c-182f-4d56-9452-4421819e0ff3"
+  subscription_id = "9485e662-9561-4893-9a75-0d40e8f34cf4"
+  client_id       = "fe82ff81-626a-43f2-8e31-fb820bcda123"
+  client_secret   = "rcF8Q~3Ui4cHtzK~4.H2LE45pOP0McT.0kFIlcyN"
+  tenant_id       = "3189c4b9-8fbc-4f43-bda6-fdba77119c88"
   features {}
 }
