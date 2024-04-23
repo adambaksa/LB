7a86e9ef917f482ce7d46ea00c1cb2a6c174643e
adambaksa <151851619+adambaksa@users.noreply.github.com>
2024-04-23 21:47:48 +0200

    Update IIS_Config.ps1

--- scripts/IIS_Config.ps1
+++ scripts/IIS_Config.ps1
@@ -3,3 +3,4 @@ Import-Module ServerManager
 Add-WindowsFeature Web-Server -IncludeAllSubFeature
 Add-WindowsFeature Web-Asp-Net45
 Add-WindowsFeature NET-Framework-Features
+Set-Content -Path "C:\inetpub\wwwroot\Default.html" -Value "This is the server $($env:computername) !"
